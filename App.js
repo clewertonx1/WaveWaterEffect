d37479a43ca6d1f4ac5ddde20e6c1e68239a34d4
Clewerton Roque <clewerton203@gmail.com>
2020-04-30 15:48:28 -0300

    Animation add

--- App.js
+++ App.js
@@ -2,50 +2,45 @@ import * as React from 'react';
 import {useEffect, useState, useRef} from 'react'
 import { Platform, StyleSheet, Text, View, Animated} from 'react-native';
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
-
 export default function App() {
 
   const animatedValue = new Animated.Value(0)
   const animatedValueRef = useRef(animatedValue)
 
   const translateX = animatedValueRef.current.interpolate({
-    inputRange: [ 0, 1],
-    outputRange: [100, 200],
+    inputRange: [ 0, 50],
+    outputRange: [0, -50],
   })
   Animated.loop(
     Animated.sequence([
       Animated.timing(animatedValue, {
-        toValue: 1,
+        toValue: 50,
         duration: 1000,  
       }),
-      Animated.delay(1000),
+      Animated.delay(100),
       Animated.timing(animatedValue, {
         toValue: 0,
-        duration: 100,  
+        duration: 1000,  
       }),
   
     ])
   ).start()
-
   
-  console.log(translateX)
+  
   return (
     <View style={styles.container}>
-      <View style={{
+      
+      <Animated.View style={{
          position: 'absolute',
          width: 400,
          height: 400,
          borderRadius: 200,
          backgroundColor: '#F5FCFF',
          zIndex: 2,
-         bottom: 330,
-         left: 13,
-      }}></View>
-       <View style={{
+         bottom: 356,
+         left: translateX,
+      }}></Animated.View>
+       <Animated.View style={{
         opacity: 0.5,
         position: 'absolute',
         width: 400,
@@ -53,9 +48,9 @@ export default function App() {
         borderRadius: 200,
         backgroundColor: '#F5FCFF',
         zIndex: 3,
-        bottom: 320,
-        left: -13,
-     }}></View>
+        bottom: 340,
+        left: translateX,
+     }}></Animated.View>
       
       <View style={styles.borderCircle}>
       <View style={styles.circle}>
